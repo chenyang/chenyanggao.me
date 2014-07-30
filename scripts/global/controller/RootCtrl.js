1541bcc00ed58fd1a9238e322371accc4df6dc5c
chenyanggao123@gmail.com <929542xcwlgc>
2014-07-30 18:26:10 +0200

    backup

--- scripts/global/controller/RootCtrl.js
+++ scripts/global/controller/RootCtrl.js
@@ -1,10 +1,24 @@
 (function(){
 	'use strict';
-	
-	var mod = angular.module('global.controller');
-	
-	mod.controller('RootCtrl', ['$scope', function($scope){
 
+	var mod = angular.module('global.controller');
+
+	mod.controller('RootCtrl', ['$scope', '$window', function($scope, $window){
+
+		//by default collapsed is false
+		$scope.collapse_menu = false;
+
+		/**
+		 * window size watch
+		 */
+		var win = angular.element($window);
+        win.bind("resize",function(e){
+        	if(win[0].innerWidth<=1024){
+        		console.log('llll');
+        		$scope.collapse_menu = true;
+        	}else{
+        		$scope.collapse_menu = false;
+        	}
+        });
 	}]);
-	
 }());
--- scripts/global/service/GlobalService.js
+++ scripts/global/service/GlobalService.js
@@ -17,46 +17,10 @@
 			 {  when:"/resume", 
 				 templateUrl:"views/resume.html"
 			 }, 
-			 {  when:"/allons_y", 
-				 templateUrl:"views/Allons-y.html"
-			 },
-			 {  when:"/asa", 
-				 templateUrl:"views/ASA.html"
-			 },
-			 {  when:"/euronext", 
-				 templateUrl:"views/Euronext.html"
-			 },
-			 {  when:"/expernova", 
-				 templateUrl:"views/Expernova.html"
-			 }, 
-			 {  when:"/findevent", 
-				 templateUrl:"views/FindEvent.html"
-			 }, 
-			 {  when:"/french_airline", 
-				 templateUrl:"views/French_Airline.html"
-			 }, 
-			 {  when:"/good_deed_movement", 
-				 templateUrl:"views/Good_Deed_Movement.html"
-			 }, 
-			 {  when:"/iovision", 
-				 templateUrl:"views/IOvision.html"
-			 }, 
-			 {  when:"/mail_system", 
-				 templateUrl:"views/Mail_System.html"
-			 }, 
-			 {  when:"/pollaroid", 
-				 templateUrl:"views/Pollaroid.html"
-			 }, 
-			 {  when:"/slab", 
-				 templateUrl:"views/SLAB.html"
-			 }, 
-			 {  when:"/stadium", 
-				 templateUrl:"views/Stadium.html"
-			 }, 
-			 {  when:"/uvsp", 
-				 templateUrl:"views/UVSP.html"
-			 }, 
-
+			 {
+				when:"/project_detail/:project",
+				templateUrl:"views/project_detail.html"
+			 }
 			 ]);
 
 
